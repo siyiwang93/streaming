25ebec5ae338f0580ebac2bd6800789220e82a4f
siyiwang93 <siyiwang93@gmail.com>
2026-03-24 00:57:24 +0000

    flink set up

---
+++ src/pass_through_job.py
@@ -0,0 +1,20 @@
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+            PULocationID INTEGER,
+            DOLocationID INTEGER,
+            trip_distance DOUBLE,
+            total_amount DOUBLE,
+            tpep_pickup_datetime BIGINT
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda:29092',
+            'topic' = 'rides',
+            'scan.startup.mode' = 'latest-offset',
+            'properties.auto.offset.reset' = 'latest',
+            'format' = 'json'
+        );
+        """
+    t_env.execute_sql(source_ddl)
+    return table_name
